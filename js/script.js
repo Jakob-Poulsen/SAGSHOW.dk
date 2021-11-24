c22b023a4f48177d5e4459ffde00bcb9a11e8f11
Jakob Poulsen <93761828+Jakob-Poulsen@users.noreply.github.com>
2021-11-25 00:10:35 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -20,11 +20,28 @@ function home(site) {
     else window.location.assign('../index.html ')
 }
 
-
 function åretsshow() {
     window.location.assign('./html/åretsshow.html ')
 }
 
-function template(år) {
-    window.location.assign('./html/shows/tidligere' + år + '.html')
+function template(år, map) {
+    if (map == '..') window.location.assign('../html/shows/tidligere' + år + '.html')
+    else window.location.assign('./html/shows/tidligere' + år + '.html')
+}
+
+function topButton() {
+    var foot = document.querySelector('footer')
+    foot.innerHTML += `
+    <button class="top-btn" onclick="returnToTop()">
+        <svg height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+            <path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/>
+        </svg>
+    </button>`;
+}
+
+topButton()
+
+function returnToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;z
 }
