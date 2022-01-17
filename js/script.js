6094d3a8b423905d23b085628db5848874b65c22
Jakob Poulsen <93761828+Jakob-Poulsen@users.noreply.github.com>
2022-01-17 20:07:26 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-console.log('connected')
+var main_title = document.getElementById('årets-titel')
+var header_title = document.getElementById('header_title')
 
+
+//*set date
+var timeConstruct = function(param) {
+    let date = new Date;
+    if (param == 'time') {
+        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    }
+    if (param == 'date') {
+        return `${date.getUTCDate()}/${date.getUTCMonth() + 1} - ${date.getFullYear()}`;
+    }
+}
+console.log(' connected...\n', timeConstruct('time') + ' | ' +  timeConstruct('date'))
+
+//*Side-menu 
 var asideBoolean = 0;
 function showMenu() {
     var btn = document.getElementById('sidemenu');
@@ -15,40 +30,46 @@ function showMenu() {
     };
 }
 
+//*footer-btn -->
+function topButton() {
+    var foot = document.querySelector('footer')
+    foot.innerHTML += `
+    <button class="top-btn" onclick="returnToTop()">
+    <svg height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+    <path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/>
+    </svg>
+    </button>`;
+}
+topButton()
+function returnToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
+//? redirection --->
 function home(site) {
-    if (site == '..') window.location.assign('../../index.html ')
-    else window.location.assign('../index.html ')
+    console.log(site)
+    if (site === '..') window.location.assign('../../index.html ')
+    if (site === '.') window.location.assign('./index.html ')
+    if (site === '../shows') window.location.assign('../index.html ')
 }
 
 function åretsshow() {
     window.location.assign('./html/åretsshow.html ')
 }
 
+
+//*link til [årstal].html
 function template(år, map) {
+    console.log(år, map)
     if (map == '..') window.location.assign('../html/shows/tidligere' + år + '.html')
     else window.location.assign('./html/shows/tidligere' + år + '.html')
 }
 
-function topButton() {
-    var foot = document.querySelector('footer')
-    foot.innerHTML += `
-    <button class="top-btn" onclick="returnToTop()">
-        <svg height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
-            <path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 8l-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14l-6-6z"/>
-        </svg>
-    </button>`;
-}
 
-topButton()
-
-function returnToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-var main_title = document.getElementById('årets-titel')
-var header_title = document.getElementById('header_title')
-
+//?indsæt div tekst -->
 header_title.innerHTML += ` <div class="after">${SAG_SHOW_ÅRSTAL}</div>`;
 
 document.querySelector('.links').childNodes[11].innerHTML = 'Køb billet!';
